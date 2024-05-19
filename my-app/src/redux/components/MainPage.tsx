9106a1c5473185eb99e6b65de67211134d9f1182
viktoria <viktoria_19_1@mail.ru>
2024-05-19 14:27:54 +0300

    made the questions appear one at a time and the results are displayed at the end

--- my-app/src/redux/components/MainPage.tsx
+++ my-app/src/redux/components/MainPage.tsx
@@ -13,36 +13,68 @@ export default function MainPage(): JSX.Element {
     (state) => state.apiSlice.questions
   );
   const [selectedAnswer, setSelectedAnswer] = useState<string>('');
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+  const [userAnswers, setUserAnswers] = useState<string[]>([]);
 
   const handleAnswerSelection = (answer: string) => {
     setSelectedAnswer(answer);
   };
 
+  const handleSubmitAnswer = () => {
+    if (selectedAnswer) {
+      setUserAnswers([...userAnswers, selectedAnswer]);
+      setSelectedAnswer('');
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      alert("Please select an answer before submitting.");
+    }
+  };
+
   return (
     <div>
-      {questions?.map((card, index) => (
-        <FormControl key={index}>
-          <FormLabel>{card.question}</FormLabel>
+      {currentQuestionIndex < questions.length ? (
+        <FormControl>
+          <FormLabel>{questions[currentQuestionIndex].question}</FormLabel>
           <RadioGroup>
-            {card.incorrect_answers.map((incorrectAnswer, i) => (
-              <Radio
-                key={i}
-                checked={selectedAnswer === incorrectAnswer}
-                onClick={() => handleAnswerSelection(incorrectAnswer)}
-                value={incorrectAnswer}
-                label={incorrectAnswer}
-              ></Radio>
-            ))}
+            {questions[currentQuestionIndex].incorrect_answers.map(
+              (incorrectAnswer, i) => (
+                <Radio
+                  key={i}
+                  checked={selectedAnswer === incorrectAnswer}
+                  onClick={() => handleAnswerSelection(incorrectAnswer)}
+                  value={incorrectAnswer}
+                  label={incorrectAnswer}
+                ></Radio>
+              )
+            )}
             <Radio
-              checked={selectedAnswer === card.correct_answer}
-              onClick={() => handleAnswerSelection(card.correct_answer)}
-              value={card.correct_answer}
-              label={card.correct_answer}
+              checked={
+                selectedAnswer ===
+                questions[currentQuestionIndex].correct_answer
+              }
+              onClick={() =>
+                handleAnswerSelection(
+                  questions[currentQuestionIndex].correct_answer
+                )
+              }
+              value={questions[currentQuestionIndex].correct_answer}
+              label={questions[currentQuestionIndex].correct_answer}
             ></Radio>
           </RadioGroup>
+          <button onClick={handleSubmitAnswer} disabled={!selectedAnswer}>
+            Submit Answer
+          </button>
         </FormControl>
-      ))}
-      <button>Submit Answer</button>
+      ) : (
+        <div>
+          <h2>User Answers:</h2>
+          <ul>
+            {userAnswers.map((answer, index) => (
+              <li key={index}>{answer}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
